dd0ad2ba066b29dba6d095165e91a79dba886cd3
RIEULoic <lrieu31@outlook.fr>
2023-09-15 15:15:27 +0400

    add clerk auth with basic config

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hello world</h1>
+      <UserButton afterSignOutUrl="/" />
+    </main>
   );
 }
